18de79784791201a3e0c49ea30fff52217d280eb
Saule Aleksandraviciute <saulealeksandraviciute@Saules-Air.lan>
2022-04-29 18:53:22 +0300

    task1 js css done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,51 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const outputField = document.getElementById('output');
+
+const convertMeasuring = (weightInKg) => {
+    const weightInLb = weightInKg * 2.2046,
+        weightInG = weightInKg / 0.0010000,
+        weightInOz = weightInKg * 35.274;
+    return { weightInLb, weightInG, weightInOz };
+}
+
+const displayWeight = (measurements) => {
+
+    outputField.replaceChildren();
+
+    const weightInLbValue = measurements.weightInLb;
+    const weightInGValue = measurements.weightInG;
+    const weightInOzValue = measurements.weightInOz;
+    const weightInKgValue = measurements.weightInKg;
+
+    const weightInLb = document.createElement('h2');
+    const weightInG = document.createElement('h2');
+    const weightInOz = document.createElement('h2');
+    const weightInKg = document.createElement('h2');
+
+    weightInKg.textContent = weightInKgValue + " Kg";
+    weightInG.textContent = weightInGValue + " g";
+    weightInOz.textContent = weightInOzValue + " Oz";
+    weightInLb.textContent = weightInLbValue + " Lb";
+
+    outputField.append(weightInKg, weightInLb, weightInG, weightInOz);
+
+}
+
+const formEl = document.querySelector('form').addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    try {
+        const weightInKg = +event.target.querySelector('#search').value;
+        const measurements = convertMeasuring(weightInKg);
+        measurements.weightInKg = weightInKg;
+        displayWeight(measurements);
+
+    } catch {
+        console.log(' error ');
+    }
+
+});
